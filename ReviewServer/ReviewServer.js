f45c31617eb0d5c4a0d3ecc6c6dc1c4a56e47404
rahuldeepak <rahuldeepak@gmail.com>
2014-08-29 19:31:22 +0530

    The files are modified. Rest is tested.

--- ReviewServer/ReviewServer.js
+++ ReviewServer/ReviewServer.js
@@ -3,21 +3,64 @@ console.log("Aum Sree Sainath");
 
 var restify = require('restify');
 var http = require("http");
+var multipart = require('multipart');
+var sys = require('sys');
 
+
+// State
+var next_user_id = 0;
+var users = {};
+
+// Server
 var server = restify.createServer({
-    name: 'myapp',
+    name: 'ReviewServer',
     version: '1.0.0'
 });
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.get('/echo/:name', function (req, res, next) {
-    res.send(req.params);
+server.get("/", function (req, res, next) {
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    res.end(JSON.stringify(users));
     return next();
 });
 
-server.listen(8082, function () {
+server.get('/user/:id', function (req, res, next) {
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    res.end(JSON.stringify(users[parseInt(req.params.id)]));
+    return next();
+});
+
+server.post('/user', function (req, res, next) {
+    var user = req.params;
+    user.id = next_user_id++;
+    users[user.id] = user;
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    res.end(JSON.stringify(user));
+    return next();
+});
+
+server.put('/user/:id', function (req, res, next) {
+    var user = users[parseInt(req.params.id)];
+    var changes = req.params;
+    delete changes.id;
+    for (var x in changes) {
+        user[x] = changes[x];
+    }
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    res.end(JSON.stringify(user));
+    return next();
+});
+
+server.del('/user/:id', function (req, res, next) {
+    delete users[parseInt(req.params.id)];
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    res.end(JSON.stringify(true));
+    return next();
+});
+
+server.listen(8013, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
 
--- TestBench/RestTest.js
+++ TestBench/RestTest.js
@@ -5,11 +5,11 @@ var assert = require('assert');
 var restify = require('restify');
 
 var client = restify.createJsonClient({
-    url: 'http://localhost:8082',
+    url: 'http://localhost:8013',
     version: '~1.0'
 });
 
-client.get('/echo/mark', function (err, req, res, obj) {
+client.get('/hello', function (err, req, res, obj) {
     assert.ifError(err);
     console.log('Server returned: %j', obj);
 });
