d25477dc46c82b5e20a72922c42bbd144ef013b3
rahuldeepak <######1Aa>
2014-09-08 22:52:15 +0530

    Server
    Server

--- ReviewServer/ReviewServer.js
+++ ReviewServer/ReviewServer.js
@@ -32,6 +32,37 @@ server.get('/user/:id', function (req, res, next) {
     return next();
 });
 
+server.post('/upload', function (req, res, next) {
+
+    console.log("Function got called");
+
+    req.setBodyEncoding('binary');
+    var stream = new multipart.Stream(req);
+
+    stream.addListener('part', function(part) {
+
+        part.addListener('body', function(chunk) {
+            var progress = (stream.bytesReceived / stream.bytesTotal * 100).toFixed(2);
+            var mb = (stream.bytesTotal / 1024 / 1024).toFixed(1);
+
+            sys.print("Uploading "+mb+"mb ("+progress+"%)\015");
+
+            // chunk could be appended to a file if the uploaded file needs to be saved
+        });
+    });
+
+
+    stream.addListener('complete', function() {
+        res.sendHeader(200, {'Content-Type': 'text/plain'});
+        res.sendBody('Thanks for playing!');
+        res.finish();
+        sys.puts("\n=> Done");
+    });
+
+
+    return next();
+});
+
 server.post('/user', function (req, res, next) {
     var user = req.params;
     user.id = next_user_id++;
